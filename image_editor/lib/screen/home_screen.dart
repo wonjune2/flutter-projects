6b7050df3ae68feefc1e94695f3d679f5da3b324
DESKTOP-36SFVP1\proten <wjlee@proten.co.kr>
2025-05-16 15:44:36 +0900

    image editor Initialize

---
+++ image_editor/lib/screen/home_screen.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_editor/component/main_app_bar.dart';
+import 'package:image_picker/image_picker.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          MainAppBar(
+            onPickImage: onPickImage,
+            onSaveImage: onSaveImage,
+            onDeleteItem: onDeleteItem,
+          ),
+          Text('Home'),
+        ],
+      ),
+    );
+  }
+
+  Widget renderBody() {
+    if (image != null) {
+      return Positioned.fill(
+        child: InteractiveViewer(
+          child: Image.file(File(image!.path), fit: BoxFit.cover),
+        ),
+      );
+    } else {
+      return Center(
+        child: TextButton(
+          style: TextButton.styleFrom(foregroundColor: Colors.white),
+          onPressed: onPickImage,
+          child: Text('이미지 선택하기'),
+        ),
+      );
+    }
+  }
+
+  void onPickImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      this.image = image;
+    });
+  }
+
+  void onSaveImage() {}
+
+  void onDeleteItem() {}
+}
